a0e7cd0c1acd1ffef4bf659e7c69005973bf4a06
d7om <hello@d7om.dev>
2026-03-18 20:16:31 +0200

    update

--- lib/providers/providers-root.tsx
+++ lib/providers/providers-root.tsx
@@ -1,21 +1,26 @@
-import { NuqsAdapter } from 'nuqs/adapters/next/app'
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 
-import { TooltipProvider } from '@/components/ui/tooltip'
+import { TooltipProvider } from "@/components/ui/tooltip";
 
-import { QueryClientProvider } from './query-client'
-import { Toaster } from './sonner-provider'
-import { ThemeProvider } from './theme-provider'
-import { AuthProvider } from '@/components/providers/auth-provider'
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+import { QueryClientProvider } from "./query-client";
+import { Toaster } from "./sonner-provider";
+import { ThemeProvider } from "./theme-provider";
+import { AuthProvider } from "@/components/providers/auth-provider";
 
 export const RootProviders = ({ children }: { children: React.ReactNode }) => (
-  <AuthProvider>
-    <NuqsAdapter>
-      <QueryClientProvider>
-        <ThemeProvider>
-          <TooltipProvider>{children}</TooltipProvider>
-        </ThemeProvider>
-      </QueryClientProvider>
-    </NuqsAdapter>
-    <Toaster />
-  </AuthProvider>
-)
+  <>
+    <AuthProvider>
+      <NuqsAdapter>
+        <QueryClientProvider>
+          <ThemeProvider>
+            <TooltipProvider>{children}</TooltipProvider>
+          </ThemeProvider>
+        </QueryClientProvider>
+      </NuqsAdapter>
+      <Toaster />
+    </AuthProvider>
+    <SpeedInsights />
+  </>
+);
